152ae4261570bab59f19f164db2e6e37b1406077
Badulundabad <greenday407@mail.ru>
2020-07-11 11:45:21 +0300

    Added the ability to select your god and target god

--- Smite Auto Attack Damage/Model/GarbageTestClass.cs
+++ Smite Auto Attack Damage/Model/GarbageTestClass.cs
@@ -1,19 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace Smite_Auto_Attack_Damage.Model
 {
-    class GarbageTestClass
+    class GarbageTestClass : INotifyPropertyChanged
     {
-        string imagePath = null;
-
-        public string ImagePath { get => imagePath; set => imagePath = value; }
-        public GarbageTestClass(string path)
+        private string name;
+        private string imagePath;
+        private string fullImagePath;
+        public string Name
         {
-            this.ImagePath = path;
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string ImagePath
+        {
+            get => imagePath;
+            set
+            {
+                imagePath = value;
+                OnPropertyChanged("ImagePath");
+            }
+        }
+        public string FullImagePath
+        {
+            get => fullImagePath;
+            set
+            {
+                fullImagePath = value;
+                OnPropertyChanged("FullImagePath");
+            }
+        }
+
+        public GarbageTestClass(string name, string imagePath)
+        {
+            this.Name = name;
+            this.ImagePath = imagePath;
+        }
+        public GarbageTestClass(string name, string imagePath, string fullImagePath)
+        {
+            this.Name = name;
+            this.ImagePath = imagePath;
+            this.FullImagePath = fullImagePath;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
         }
     }
 }
--- Smite Auto Attack Damage/View/MainWindow.xaml.cs
+++ Smite Auto Attack Damage/View/MainWindow.xaml.cs
@@ -1,19 +1,11 @@
 ﻿using Smite_Auto_Attack_Damage.Model;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Effects;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Smite_Auto_Attack_Damage
 {
@@ -22,35 +14,265 @@ namespace Smite_Auto_Attack_Damage
     /// </summary>
     public partial class MainWindow : Window
     {
+        static bool[] godSelectedItemsStateArray = new bool[7];
+        static bool[] targetSelectedItemsStateArray = new bool[7];
+        static bool[] godDeletingItemsArray = new bool[7];
+        static bool[] targetDeletingItemsArray = new bool[7];
+
         public MainWindow()
         {
             InitializeComponent();
-            Characteristics[] stats = new Characteristics[] { new Characteristics("/Images/Stats/HandDamage.png", 200) };
-            attackerTestListBox.ItemsSource = stats;
-            targetTestListBox.ItemsSource = stats;
+
+            var testGod = new GarbageTestClass[1];
+            testGod[0] = new GarbageTestClass("n a m e", "/Images/logo.png", "/Images/logo.png");
             var sixItems = new GarbageTestClass[6];
             for (int i = 0; i < sixItems.Length; i++)
             {
-                sixItems[i] = new GarbageTestClass("/Images/Items/bloodforge.png");
+                sixItems[i] = new GarbageTestClass("n a m e", "/Images/logo.png");
             }
             var manyItems = new GarbageTestClass[50];
-            for (int i = 0; i < sixItems.Length; i++)
+            for (int i = 0; i < manyItems.Length; i++)
             {
-                manyItems[i] = new GarbageTestClass("/Images/Items/bloodforge.png");
+                if ((i + 2) % 2 == 0)
+                {
+                    manyItems[i] = new GarbageTestClass("heartseeker", "/Images/Items/heartseeker.png");
+                }
+                else manyItems[i] = new GarbageTestClass("soulreaver", "/Images/Items/soulreaver.png");
             }
-
-            godsListsItems.ItemsSource = manyItems;
-            targetsListsItems.ItemsSource = manyItems;
-
-            godsItems.ItemsSource = sixItems;
-            targetsItems.ItemsSource = sixItems;
-
-            godBuildsItems.ItemsSource = sixItems;
-            //items.ItemsSource = sixItems;
             var manyZeuses = new GarbageTestClass[100];
             for (int i = 0; i < manyZeuses.Length; i++)
             {
-                manyZeuses[i] = new GarbageTestClass("/Images/GodIcons/zeus.png");
+                if ((i + 2) % 2 == 0)
+                {
+                    manyZeuses[i] = new GarbageTestClass(
+                        "z e u s",
+                        "/Images/GodIcons/zeus - mini.jpg",
+                        "/Images/Gods/zeus.jpg");
+                }
+                else manyZeuses[i] = new GarbageTestClass(
+                    "h a c h i m a n",
+                    "/Images/GodIcons/hachiman-mini.jpg",
+                    "/Images/Gods/hachiman.jpg");
+            }
+            Characteristics[] stats = new Characteristics[] { new Characteristics("/Images/Stats/HandDamage.png", 200) };
+
+            god.ItemsSource = testGod;
+            godsItems.ItemsSource = sixItems;
+            builds.ItemsSource = sixItems;
+            godsItemsList.ItemsSource = manyItems;
+            godsList.ItemsSource = manyZeuses;
+            godsStatistics.ItemsSource = stats;
+
+
+            var testTarget = new GarbageTestClass[1];
+            testTarget[0] = new GarbageTestClass("n a m e", "/Images/logo.png", "/Images/logo.png");
+            var targetsSixItems = new GarbageTestClass[6];
+            for (int i = 0; i < targetsSixItems.Length; i++)
+            {
+                targetsSixItems[i] = new GarbageTestClass("n a m e", "/Images/logo.png");
+            }
+            var targetsManyItems = new GarbageTestClass[50];
+            for (int i = 0; i < targetsManyItems.Length; i++)
+            {
+                if ((i + 2) % 2 == 0)
+                {
+                    targetsManyItems[i] = new GarbageTestClass("heartseeker", "/Images/Items/heartseeker.png");
+                }
+                else targetsManyItems[i] = new GarbageTestClass("soulreaver", "/Images/Items/soulreaver.png");
+            }
+            var targetsManyZeuses = new GarbageTestClass[100];
+            for (int i = 0; i < targetsManyZeuses.Length; i++)
+            {
+                if ((i + 2) % 2 == 0)
+                {
+                    targetsManyZeuses[i] = new GarbageTestClass(
+                        "z e u s",
+                        "/Images/GodIcons/zeus - mini.jpg",
+                        "/Images/Gods/zeus.jpg");
+                }
+                else targetsManyZeuses[i] = new GarbageTestClass(
+                    "h a c h i m a n",
+                    "/Images/GodIcons/hachiman-mini.jpg",
+                    "/Images/Gods/hachiman.jpg");
+            }
+
+            target.ItemsSource = testTarget;
+            targetsItems.ItemsSource = targetsSixItems;
+            targetsItemsList.ItemsSource = targetsManyItems;
+            targetsList.ItemsSource = targetsManyZeuses;
+            targetsStatistics.ItemsSource = stats;
+        }
+
+
+        private void Window_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.Source.GetType().Name != "ListBox")
+            {
+                if (godsItems.SelectedIndex != -1)
+                {
+                    godSelectedItemsStateArray[godsItems.SelectedIndex] = false;
+                    ListBoxItem container =
+                        godsItems.ItemContainerGenerator.ContainerFromItem(godsItems.SelectedItem)
+                        as ListBoxItem;
+                    container.IsSelected = false;
+                }
+                if (godsItemsList.SelectedIndex != -1)
+                {
+                    ListBoxItem container =
+                        godsItemsList.ItemContainerGenerator.ContainerFromItem(godsItemsList.SelectedItem)
+                        as ListBoxItem;
+                    container.IsSelected = false;
+                }
+                if (god.SelectedIndex != -1)
+                {
+                    ListBoxItem container =
+                        god.ItemContainerGenerator.ContainerFromIndex(god.SelectedIndex)
+                        as ListBoxItem;
+                    container.IsSelected = false;
+                }
+                if (targetsItems.SelectedIndex != -1)
+                {
+                    targetSelectedItemsStateArray[targetsItems.SelectedIndex] = false;
+                    ListBoxItem container =
+                        targetsItems.ItemContainerGenerator.ContainerFromItem(targetsItems.SelectedItem)
+                        as ListBoxItem;
+                    container.IsSelected = false;
+                }
+                if (targetsItemsList.SelectedIndex != -1)
+                {
+                    ListBoxItem container =
+                        targetsItemsList.ItemContainerGenerator.ContainerFromItem(targetsItemsList.SelectedItem)
+                        as ListBoxItem;
+                    container.IsSelected = false;
+                }
+                if (target.SelectedIndex != -1)
+                {
+                    ListBoxItem container =
+                        target.ItemContainerGenerator.ContainerFromIndex(target.SelectedIndex)
+                        as ListBoxItem;
+                    container.IsSelected = false;
+                }
+                godsItemsList.Visibility = Visibility.Hidden;
+                targetsItemsList.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void selectedItem_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (godsItems.SelectedIndex != -1)
+            {
+                if (!godSelectedItemsStateArray[godsItems.SelectedIndex])
+                {
+                    for (int number = 0; number < godSelectedItemsStateArray.Count(); number++)
+                        godSelectedItemsStateArray[number] = false;
+                    godSelectedItemsStateArray[godsItems.SelectedIndex] = true;
+                    godsItemsList.Visibility = Visibility.Visible;
+                }
+                else if (godSelectedItemsStateArray[godsItems.SelectedIndex])
+                {
+                    for (int number = 0; number < godSelectedItemsStateArray.Count(); number++)
+                        godSelectedItemsStateArray[number] = false;
+                    godDeletingItemsArray[godsItems.SelectedIndex] = true;
+                }
+                if (godDeletingItemsArray[godsItems.SelectedIndex])
+                {
+                    godDeletingItemsArray[godsItems.SelectedIndex] = false;
+                    GarbageTestClass selectedItem = godsItems.SelectedItem as GarbageTestClass;
+                    selectedItem.ImagePath = "/Images/logo.png";
+                    selectedItem = new GarbageTestClass("n a m e", "/Images/logo.png");
+                    ListBoxItem container = new ListBoxItem();
+                    container = godsItems.ItemContainerGenerator.ContainerFromItem(godsItems.SelectedItem) as ListBoxItem;
+                    container.IsSelected = false;
+                    godsItemsList.Visibility = Visibility.Hidden;
+                }
+            }
+            if (targetsItems.SelectedIndex != -1)
+            {
+                if (!targetSelectedItemsStateArray[targetsItems.SelectedIndex])
+                {
+                    for (int number = 0; number < targetSelectedItemsStateArray.Count(); number++)
+                        targetSelectedItemsStateArray[number] = false;
+                    targetSelectedItemsStateArray[targetsItems.SelectedIndex] = true;
+                    targetsItemsList.Visibility = Visibility.Visible;
+                }
+                else if (targetSelectedItemsStateArray[targetsItems.SelectedIndex])
+                {
+                    for (int number = 0; number < targetSelectedItemsStateArray.Count(); number++)
+                        targetSelectedItemsStateArray[number] = false;
+                    targetDeletingItemsArray[targetsItems.SelectedIndex] = true;
+                }
+                if (targetDeletingItemsArray[targetsItems.SelectedIndex])
+                {
+                    targetDeletingItemsArray[targetsItems.SelectedIndex] = false;
+                    GarbageTestClass selectedItem = targetsItems.SelectedItem as GarbageTestClass;
+                    selectedItem.ImagePath = "/Images/logo.png";
+                    selectedItem = new GarbageTestClass("n a m e", "/Images/logo.png");
+                    ListBoxItem container = new ListBoxItem();
+                    container = targetsItems.ItemContainerGenerator.ContainerFromItem(targetsItems.SelectedItem) as ListBoxItem;
+                    container.IsSelected = false;
+                    targetsItemsList.Visibility = Visibility.Hidden;
+                }
+
+            }
+        }
+
+        private void listsItem_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if(godsItems.SelectedIndex != -1)
+            {
+                godSelectedItemsStateArray[godsItems.SelectedIndex] = false;
+                GarbageTestClass selectedItem = godsItems.SelectedItem as GarbageTestClass;
+                GarbageTestClass listsItem = godsItemsList.SelectedItem as GarbageTestClass;
+                selectedItem.ImagePath = listsItem.ImagePath;
+                selectedItem = listsItem;
+                ListBoxItem container = new ListBoxItem();
+                container = godsItems.ItemContainerGenerator.ContainerFromItem(godsItems.SelectedItem) as ListBoxItem;
+                container.IsSelected = false;
+                container = godsItemsList.ItemContainerGenerator.ContainerFromItem(listsItem) as ListBoxItem;
+                container.IsSelected = false;
+                godsItemsList.Visibility = Visibility.Hidden;
+            }
+            if(targetsItems.SelectedIndex != -1)
+            {
+                targetSelectedItemsStateArray[targetsItems.SelectedIndex] = false;
+                GarbageTestClass selectedItem = targetsItems.SelectedItem as GarbageTestClass;
+                GarbageTestClass listsItem = targetsItemsList.SelectedItem as GarbageTestClass;
+                selectedItem.ImagePath = listsItem.ImagePath;
+                selectedItem = listsItem;
+                ListBoxItem container = new ListBoxItem();
+                container = targetsItems.ItemContainerGenerator.ContainerFromItem(targetsItems.SelectedItem) as ListBoxItem;
+                container.IsSelected = false;
+                container = targetsItemsList.ItemContainerGenerator.ContainerFromItem(listsItem) as ListBoxItem;
+                container.IsSelected = false;
+                targetsItemsList.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void listsGod_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (god.SelectedIndex == 0)
+            {
+                GarbageTestClass selectedGod = god.SelectedItem as GarbageTestClass;
+                GarbageTestClass listsGod = godsList.SelectedItem as GarbageTestClass;
+                selectedGod.FullImagePath = listsGod.FullImagePath;
+                selectedGod = listsGod;
+                godsName.Text = selectedGod.Name;
+                ListBoxItem container = godsList.ItemContainerGenerator.ContainerFromItem(listsGod) as ListBoxItem;
+                container.IsSelected = false;
+                container = god.ItemContainerGenerator.ContainerFromIndex(0) as ListBoxItem;
+                container.IsSelected = false;
+            }
+            if(target.SelectedIndex == 0)
+            {
+                GarbageTestClass selectedGod = target.SelectedItem as GarbageTestClass;
+                GarbageTestClass listsGod = targetsList.SelectedItem as GarbageTestClass;
+                selectedGod.FullImagePath = listsGod.FullImagePath;
+                selectedGod = listsGod;
+                targetsName.Text = selectedGod.Name;
+                ListBoxItem container = targetsList.ItemContainerGenerator.ContainerFromItem(listsGod) as ListBoxItem;
+                container.IsSelected = false;
+                container = target.ItemContainerGenerator.ContainerFromIndex(0) as ListBoxItem;
+                container.IsSelected = false;
             }
         }
     }
