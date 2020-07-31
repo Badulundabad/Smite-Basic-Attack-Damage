d52b3922996b3b40439dbfe3a73ff44b33698b5d
NotABugAbuser <jenyagvozd@mail.ru>
2020-07-31 20:19:30 +0300

    Revert "Some arrays are converted to collections"
    This reverts commit 464f91107f8ced742981a13ca6c39e3cc3a18a34.

--- Smite Auto Attack Damage/Calculation.cs
+++ Smite Auto Attack Damage/Calculation.cs
@@ -5,22 +5,43 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
 using System.ComponentModel;
-using System.Collections.ObjectModel;
 
 namespace Smite_Auto_Attack_Damage 
 {
-    class Data : INotifyPropertyChanged
+    class Calculation : INotifyPropertyChanged
     {
-        readonly static God zeroGod = new God(0, "G o d", "/Images/God/Preview/fullLogo.png", null, "Physical", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-        static ObservableCollection<God> currentAttacker = new ObservableCollection<God> { zeroGod };
+        static God zeroGod = new God(0, "G o d", "/Images/God/Preview/fullLogo.png", null, "Physical", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        static God[] currentAttacker = new God[1] { zeroGod };
 
-        readonly static Item zeroItem = new Item(0, 0, 0, null, "/Images/logo.png", null, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        static int itemOfAttackerIndex = 0;
+        static int itemOfTargetIndex = 0;
+
+        static Item zeroItem = new Item(0, 0, 0, null, "/Images/logo.png", null, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        static Item[] itemsOfAttacker = new Item[6] { zeroItem, zeroItem, zeroItem, zeroItem, zeroItem, zeroItem};
+        static Item[] itemsOfTarget = new Item[6] { zeroItem, zeroItem, zeroItem, zeroItem, zeroItem, zeroItem };
 
         static Item[] listOfPhysicalItems = new Item[114];
         static Item[] listOfMagicalItems = new Item[114];
 
-        static ObservableCollection<Item> sixItems_attacker = new ObservableCollection<Item> { zeroItem, zeroItem, zeroItem, zeroItem, zeroItem, zeroItem};
+        static Item[] zeroItemArray = new Item[6] { ZeroItem, ZeroItem, ZeroItem, ZeroItem, ZeroItem, ZeroItem };
+        static Item[] sixItems_attacker = new Item[6];
+        static Item[] sixItems_target = ZeroItemArray;
+
         static Characteristic characteristics_attacker = new Characteristic();
+        public static int ItemOfAttackerIndex
+        {
+            get
+            {
+                return itemOfAttackerIndex;
+            }
+            set
+            {
+                if(value >= 0 && value <= 5)
+                {
+                    itemOfAttackerIndex = value;
+                }
+            }
+        }
 
         static public Characteristic Characteristics_Attacker
         {
@@ -49,7 +70,7 @@ namespace Smite_Auto_Attack_Damage
             }
         }
         
-        static public ObservableCollection<God> CurrentAttacker { 
+        static public God[] CurrentAttacker { 
             get => currentAttacker; 
             set
             {
@@ -59,7 +80,8 @@ namespace Smite_Auto_Attack_Damage
 
         internal static Item[] ListOfPhysicalItems { get => listOfPhysicalItems; set => listOfPhysicalItems = value; }
         internal static Item[] ListOfMagicalItems { get => listOfMagicalItems; set => listOfMagicalItems = value; }
-        internal static ObservableCollection<Item> SixItems_attacker { get => sixItems_attacker; set => sixItems_attacker = value; }
+        internal static Item[] ZeroItemArray { get => zeroItemArray; set => zeroItemArray = value; }
+        internal static Item[] SixItems_attacker { get => sixItems_attacker; set => sixItems_attacker = value; }
 
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string prop = "")
